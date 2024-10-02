6cc5cd20cbf4e9b065d8557e03e9f351f720794d
zxchrstn <deness.christianbenjamin@gmail.com>
2024-10-02 17:34:24 +0800

    fixed app container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,3 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import { Container } from "react-bootstrap";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import HomeScreen from "./screens/Homescreen";
@@ -11,7 +9,7 @@ function App() {
   return (
     <Router>
       <main className="py-3">
-        <Container>
+        <Container fluid>
           <Routes>
             <Route path="/" element={<HomeScreen />}/>
             <Route path="/login" element={<LoginScreen />}/>
